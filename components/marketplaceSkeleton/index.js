6705da62e00775c1dc520bb534ac03d54b4b1da4
wpalani <abdulrahmanalani95@gmail.com>
2024-05-27 16:10:16 -0700

    Fix `MarketplaceSkeleton` component eslint

--- components/marketplaceSkeleton/index.js
+++ components/marketplaceSkeleton/index.js
@@ -2,21 +2,22 @@ import './stylesheet.scss';
 
 /**
  * MarketplaceSkeleton Component
- * Use to generate content skeleton
- * 
- * @param {*} props 
- * @returns 
+ * Use to generate content loading skeleton
+ *
+ * @param {*} props
+ * @return {JSX.Element} MarketplaceSkeleton
  */
-const MarketplaceSkeleton = ({ width, height, customClass }) => {
-    return ( 
-        <div 
-            className={ "newfold-marketplace-skeleton " + ( customClass ) }
-            style={{
-                "width": width || "100%",
-                "height": height || "auto"
-            }}>
-        </div>
-     );
-}
- 
+const MarketplaceSkeleton = ( { width, height, customClass } ) => {
+	return (
+		<div
+			// eslint-disable-next-line prettier/prettier
+			className={ 'newfold-marketplace-skeleton ' + ( customClass ) }
+			style={ {
+				width: width || '100%',
+				height: height || 'auto',
+			} }
+		></div>
+	);
+};
+
 export default MarketplaceSkeleton;
