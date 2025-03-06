b207dbf1dde34ca427d3093c3e87a6e3f1156fd8
Vara <varaprasadmaruboina@gmail.com>
2025-03-06 21:28:06 +0530

    Add filter X-WP-LOCALE for the Hiive API calls

--- includes/Marketplace.php
+++ includes/Marketplace.php
@@ -29,6 +29,25 @@ class Marketplace {
 		add_action( 'rest_api_init', array( MarketplaceApi::class, 'registerRoutes' ) );
 		add_action( 'wp_loaded', array( Themes::class, 'init' ) );
 		add_action( 'wp_loaded', array( PluginsMarketplace::class, 'init' ) );
+		add_filter( 'http_request_args', array( __CLASS__, 'add_locale_header' ), 10, 2 );
 	}
 
+	/**
+	 * Add locale headers to hiive requests.
+	 *
+	 * @param array  $args - HTTP request arguments
+	 * @param string $url - URL of the request
+	 *
+	 * @return array
+	 */
+	public static function add_locale_header( $args, $url ) {
+		if ( defined( 'NFD_HIIVE_URL' ) && strpos( $url, NFD_HIIVE_URL ) !== false ) {
+			if ( ! isset( $args['headers'] ) || ! is_array( $args['headers'] ) ) {
+				$args['headers'] = array();
+			}
+			$args['headers']['X-WP-LOCALE'] = get_locale();
+		}
+
+		return $args;
+	}
 }
