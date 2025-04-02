90fb53102b08b0b1c53bb523765e3de83d434a07
Evan Mullins <evanrm@gmail.com>
2025-04-02 13:37:19 -0400

    add sidebar component for marketplace categories

---
+++ components/marketplaceSubnav/Sidebar.js
@@ -0,0 +1,41 @@
+import { SidebarNavigation, Title } from '@newfold/ui-component-library';
+
+export const Sidebar = ( { categories, activeCategoryIndex } ) => {
+	const activePath = activeCategoryIndex
+		? categories[ activeCategoryIndex ].name
+		: 'featured';
+	return (
+		<aside className={ 'nfd-marketplace-sidebar' }>
+			<SidebarNavigation activePath={ activePath }>
+				<SidebarNavigation.Sidebar className="nfd-min-w-60">
+					<SidebarNavigation.MenuItem
+						label="Categories"
+						defaultOpen={ true }
+						className={ 'nfd-hidden' }
+					>
+						<Title
+							as="h4"
+							className="nfd-marketplace-categories-title nfd-mb-4"
+						>
+							{ __( 'Categories', 'wp-module-marketplace' ) }
+						</Title>
+						{ categories.map( ( cat ) => (
+							<SidebarNavigation.SubmenuItem
+								pathProp="id"
+								id={ cat.name }
+								label={
+									cat.title +
+									( cat?.products_count
+										? ` (${ cat.products_count })`
+										: '' )
+								}
+								key={ cat.name }
+								href={ `#/marketplace/${ cat.name }` }
+							/>
+						) ) }
+					</SidebarNavigation.MenuItem>
+				</SidebarNavigation.Sidebar>
+			</SidebarNavigation>
+		</aside>
+	);
+};
